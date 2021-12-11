226e86562822d1f2d8785132e91c5afa4264bee1
Martin Lazarov <mlazarov@dxc.com>
2021-12-11 17:29:35 +0200

    initial commit

---
+++ functions/load_data/app.py
@@ -0,0 +1,47 @@
+import os
+import json
+import boto3
+import logging
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+ddbclient = boto3.client('dynamodb', endpoint_url='http://dynamodb:8000/')
+ 
+
+def lambda_handler(event, context):
+    try:
+        response = ddbclient.batch_write_item(
+        RequestItems={
+            os.environ['DDBTableName']: [
+                {
+                    'PutRequest': {
+                        'Item': {
+                            'make': {'S': 'Nissan'},
+                            'model': {'S': 'Micra'},
+                            'year': {'N': 2004},
+                            'chassis_no': {'S': '12345A'},
+                            'id': {'N': '1'},
+                            'last_updated': {'S': ''},
+                            'price': {'N': 500.0}
+                        }
+                    }
+                }
+            ]}
+        )
+
+        return {
+            'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
+            'body': json.dumps({
+                'message': 'Filled DynamoDB',
+            }),
+        }
+
+    except ddbclient.exceptions.ResourceNotFoundException as e:
+        logging.error('Cannot do operations on a non-existent table')
+        raise e
+    except ClientError as e:
+        logging.error('Unexpected error')
+        raise e
+
