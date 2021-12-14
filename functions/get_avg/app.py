6d06eb3053f7eb5428a17e8c21b6fd68bec136de
Martin Lazarov <mlazarov@dxc.com>
2021-12-14 21:04:25 +0200

    refactoring

--- functions/get_avg/app.py
+++ functions/get_avg/app.py
@@ -1,5 +1,5 @@
 import os
-import simplejson as json # type: ignore
+import simplejson as json  # type: ignore
 import boto3
 import logging
 from botocore.exceptions import ClientError
@@ -7,45 +7,48 @@ from boto3.dynamodb.types import NULL, TypeDeserializer
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-ddbclient = boto3.client('dynamodb', endpoint_url='http://dynamodb:8000/')
-TABLENAME = os.environ['DDBTableName']
+ddbclient = boto3.client("dynamodb", endpoint_url="http://dynamodb:8000/")
+TABLENAME = os.environ["DDBTableName"]
+
 
 def lambda_handler(event, context):
-    
+
     try:
         response = ddbclient.scan(
             TableName=TABLENAME,
-            Select='SPECIFIC_ATTRIBUTES',
-            ProjectionExpression='price',
+            Select="SPECIFIC_ATTRIBUTES",
+            ProjectionExpression="price",
         )
-        
-        #logging.info(response)
-        items = response['Items']
+
+        # logging.info(response)
+        items = response["Items"]
         if len(items) > 0:
-            lst = [TypeDeserializer().deserialize(x['price']) for x in items if x]  # type: ignore 
-            avg = sum(lst)/len(lst)
+            lst = [TypeDeserializer().deserialize(x["price"]) for x in items if x]  # type: ignore
+            avg = sum(lst) / len(lst)
             return {
-                'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
-                'headers': { 
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin':'*'
+                "statusCode": response["ResponseMetadata"]["HTTPStatusCode"],
+                "headers": {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*",
                 },
-                'body': json.dumps(avg)
+                "body": json.dumps({"message": f"Average price is : {avg}"}),
             }
         else:
             return {
-                'statusCode': '404',
-                'headers': { 
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin':'*'
+                "statusCode": "404",
+                "headers": {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*",
                 },
-                'body': json.dumps({
-                    'message': 'Item not found',
-                }),
+                "body": json.dumps(
+                    {
+                        "message": "Item not found",
+                    }
+                ),
             }
     except ddbclient.exceptions.ResourceNotFoundException as e:
-        logging.error('Cannot do operations on a non-existent table')
+        logging.error("Cannot do operations on a non-existent table")
         raise e
     except ClientError as e:
-        logging.error('Unexpected error')
+        logging.error("Unexpected error")
         raise e
